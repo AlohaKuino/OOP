81d8ab545dbc1981b84ce4b0682680a897692b9a
AlohaKuino <kolyasashatwo@gmail.com>
2023-09-12 15:29:18 +0700

    finalll

--- Task_1_1_1/src/test/java/ru/nsu/shushakov/heapsort/HeapSortTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/shushakov/heapsort/HeapSortTest.java
@@ -1,17 +1,12 @@
 package ru.nsu.shushakov.heapsort;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Test;
+import java.util.Random;
+import java.util.Arrays;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class HeapSortTest {
-    @AfterEach
-    void tearDown() {
-        System.out.println("nice");
-    }
-
     @Test
     void easySort() {
         assertArrayEquals(new int[] {1, 2, 3, 4}, HeapSort.sort(new int[] {3, 1, 2, 4}));
@@ -21,14 +16,36 @@ class HeapSortTest {
     void hardSort() {
         assertArrayEquals(new int[] {-10, -2, 2, 5, 7, 100}, HeapSort.sort(new int[] {-10, 7, 100, 2, -2, 5}));
     }
+    @Test
     void emptySort() {
         assertArrayEquals(new int[] {}, HeapSort.sort(new int[] {}));
     }
+    @Test
     void zeroSort() {
         assertArrayEquals(new int[] {0, 0, 0}, HeapSort.sort(new int[] {0, 0, 0}));
     }
-    @AfterAll
-    static void end(){
-        System.out.println("hehehehihihihahaha");
+    @Test
+    void compareSort() {
+        int[] arr = {23, 45, 6, -1, 3, 4, -10, 6, 14, 8, 1, 67, -190, 34, 23, 1, 0, -100};
+        Arrays.sort(arr);
+        int[] copyArr = {23, 45, 6, -1, 3, 4, -10, 6, 14, 8, 1, 67, -190, 34, 23, 1, 0, -100};
+        assertArrayEquals (arr, HeapSort.sort(copyArr));
+    }
+    @Test
+    void largeSort(){
+        int length = 1000; // длина последовательности
+        int min = -1000; // минимальное значение числа
+        int max = 1000; // максимальное значение числа
+
+        Random random = new Random();
+        int[] arr = new int[length];
+        int[] copyArr = new int[length];
+        for (int i = 0; i < length; i++) {
+            int number = random.nextInt(max - min + 1) + min;
+            arr[i] = number;
+            copyArr[i] = number;
+        }
+        Arrays.sort(arr);
+        assertArrayEquals (arr, HeapSort.sort(copyArr));
     }
 }
