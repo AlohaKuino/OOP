acc64cc282a38e5ad3b5df3e56048c9b9df8fc47
AlohaKuino <kolyasashatwo@gmail.com>
2023-09-12 17:56:51 +0700

    daft punk

--- Task_1_1_1/src/test/java/ru/nsu/shushakov/heapsort/HeapSortTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/shushakov/heapsort/HeapSortTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 import java.util.Arrays;
 import java.util.Random;
-
 import org.junit.jupiter.api.Test;
 
 class HeapSortTest {
@@ -19,25 +18,28 @@ class HeapSortTest {
         assertArrayEquals(new int[] {-10, -2, 2, 5, 7, 100},
                 HeapSort.sort(new int[] {-10, 7, 100, 2, -2, 5}));
     }
+
     @Test
     void emptySort() {
         assertArrayEquals(new int[] {},
                 HeapSort.sort(new int[] {}));
     }
+
     @Test
     void zeroSort() {
         assertArrayEquals(new int[] {0, 0, 0},
                 HeapSort.sort(new int[] {0, 0, 0}));
     }
+
     @Test
     void compareSort() {
         int[] arr = {23, 45, 6, -1, 3, 4, -10, 6, 14, 8, 1, 67, -190, 34, 23, 1, 0, -100};
         Arrays.sort(arr);
         int[] copyArr = {23, 45, 6, -1, 3, 4, -10, 6, 14, 8, 1, 67, -190, 34, 23, 1, 0, -100};
-        assertArrayEquals (arr, HeapSort.sort(copyArr));
+        assertArrayEquals(arr, HeapSort.sort(copyArr));
     }
     @Test
-    void largeSort(){
+    void largeSort() {
         int length = 1000; // длина последовательности
         int min = -1000; // минимальное значение числа
         int max = 1000; // максимальное значение числа
@@ -51,6 +53,6 @@ class HeapSortTest {
             copyArr[i] = number;
         }
         Arrays.sort(arr);
-        assertArrayEquals (arr, HeapSort.sort(copyArr));
+        assertArrayEquals(arr, HeapSort.sort(copyArr));
     }
 }
