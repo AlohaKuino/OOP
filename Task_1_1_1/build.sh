0bf43af57769f5ad632c60425164229ef87d2b1e
AlohaKuino <kolyasashatwo@gmaol.com>
2023-09-14 14:15:35 +0700

    he

--- Task_1_1_1/build.sh
+++ Task_1_1_1/build.sh
@@ -1,4 +1,4 @@
-javadoc -d ./build/doc/javadoc src/main/java/ru/nsu/shushakov/heapsort/HeapSort.java
+javadoc -d ./build/docs/javadoc src/main/java/ru/nsu/shushakov/heapsort/HeapSort.java
 javac -d ./build src/main/java/ru/nsu/shushakov/heapsort/HeapSort.java
 java -classpath ./build ru.nsu.shushakov.heapsort.HeapSort
 echo heehehehehehehe
