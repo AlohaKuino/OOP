94f1664d495bb581e4fe1c20d0550b6a24942a5c
AlohaKuino <kolyasashatwo@gmail.com>
2023-09-14 11:32:31 +0700

    daft punk

---
+++ Task_1_1_1/script.bat
@@ -0,0 +1,8 @@
+javadoc -d ./build/doc
+src/main/java/ru/nsu/shushakov/heapsort/HeapSort.java
+javac -d ./build
+src/main/java/ru/nsu/shushakov/heapsort/HeapSort.java
+java -classpath ./build
+ru/nsu/shushakov/heapsort/HeapSort
+echo heehehehehehehe
+
