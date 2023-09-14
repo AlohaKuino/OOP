6d096941dc75544222d00d62b603a272a9ea70c3
AlohaKuino <kolyasashatwo@gmaol.com>
2023-09-14 13:53:07 +0700

    he

---
+++ Task_1_1_1/build.sh
@@ -0,0 +1,5 @@
+javadoc -d ./build/doc/javadoc src/main/java/ru/nsu/shushakov/heapsort/HeapSort.java
+javac -d ./build src/main/java/ru/nsu/shushakov/heapsort/HeapSort.java
+java -classpath ./buildru/nsu/shushakov/heapsort/HeapSort
+echo heehehehehehehe
+
--- Task_1_1_1/script.bat
+++
@@ -1,8 +0,0 @@
-javadoc -d ./build/doc
-src/main/java/ru/nsu/shushakov/heapsort/HeapSort.java
-javac -d ./build
-src/main/java/ru/nsu/shushakov/heapsort/HeapSort.java
-java -classpath ./build
-ru/nsu/shushakov/heapsort/HeapSort
-echo heehehehehehehe
-
