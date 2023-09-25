c5d94df0a907593828dd0eb28c05eca25e4681bc
AlohaKuino <kolyasashatwo@gmaol.com>
2023-09-25 15:14:18 +0700

    idea killer

---
+++ Task_1_1_2/src/main/java/ru/nsu/shushakov/Polinomial.java
@@ -0,0 +1,188 @@
+package ru.nsu.shushakov;
+
+import java.util.Arrays;
+
+/**
+ * main class.
+ *
+ * <p>
+ *     polynomCof - array of polynom's coefficients.
+ *     pow - the greatest polynim's power.
+ * </p>
+ */
+public class Polinomial {
+    int[] polynomCof;
+    int pow;
+
+    /**
+     * constructor of class Polinomial.
+     *
+     * @param cofs - we take int[] to make a polynom.
+     */
+    public Polinomial(int[] cofs){
+        this.polynomCof = cofs;
+        this.pow = cofs.length;
+    }
+
+    /**
+     * adding one polynom to another.
+     *
+     * @param pol2 - second polynom.
+     *
+     * @return
+     */
+    public Polinomial plus(Polinomial pol2){
+        int[] newPol = new int[Math.max(this.pow, pol2.pow)];
+        for(int i = 0; i < Math.min(this.pow, pol2.pow); i ++){
+            newPol[i] =  this.polynomCof[i] + pol2.polynomCof[i];
+        }
+        if(this.polynomCof.length > pol2.polynomCof.length) {
+            for (int i = Math.min(this.pow, pol2.pow); i < Math.max(this.pow, pol2.pow); i++) {
+                newPol[i] = this.polynomCof[i];
+            }
+        }
+        else{
+            for (int i = Math.min(this.pow, pol2.pow); i < Math.max(this.pow, pol2.pow); i++) {
+                newPol[i] = pol2.polynomCof[i];
+            }
+        }
+        Polinomial retPol = new Polinomial(newPol);
+        return  retPol;
+    }
+
+    /**
+     * substract one polynom from another.
+     *
+     * @param pol2 - second polynom.
+     *
+     * @return
+     */
+    public Polinomial minus(Polinomial pol2){
+        int[] newPol = new int[Math.max(this.pow, pol2.pow)];
+        for(int i = 0; i < Math.min(this.pow, pol2.pow); i ++){
+            newPol[i] =  this.polynomCof[i] - pol2.polynomCof[i];
+        }
+        if(this.polynomCof.length > pol2.polynomCof.length) {
+            for (int i = Math.min(this.pow, pol2.pow); i < Math.max(this.pow, pol2.pow); i++) {
+                newPol[i] = this.polynomCof[i];
+            }
+        }
+        else{
+            for (int i = Math.min(this.pow, pol2.pow); i < Math.max(this.pow, pol2.pow); i++) {
+                newPol[i] = -pol2.polynomCof[i];
+            }
+        }
+        Polinomial retPol = new Polinomial(newPol);
+        return  retPol;
+    }
+
+    /**
+     * multiply one polynom to another.
+     *
+     * @param pol2 - second polynom.
+     *
+     * @return
+     */
+    public Polinomial mult(Polinomial pol2){
+        if(this.polynomCof.length == 0 && pol2.polynomCof.length == 0)
+            return pol2;
+        else if(this.polynomCof.length == 0)
+            return pol2;
+        else if(pol2.polynomCof.length == 0)
+            return this;
+
+        int[] newPol = new int[this.polynomCof.length + pol2.polynomCof.length - 1];
+        for(int i = 0; i < this.polynomCof.length; i ++){
+            for (int j = 0; j < pol2.polynomCof.length; j ++) {
+                newPol[i + j] += this.polynomCof[i] * pol2.polynomCof[j];
+            }
+        }
+        Polinomial retPol = new Polinomial(newPol);
+        return  retPol;
+    }
+
+    /**
+     * gives value in spot.
+     *
+     * @param spot - point that we replace x with.
+     *
+     * @return
+     */
+    public int inSpot(int spot){
+        int resInSpot = this.polynomCof[0];
+        for(int i = 1; i < this.polynomCof.length; i ++){
+            resInSpot += Math.pow(spot, i) * this.polynomCof[i];
+        }
+        return resInSpot;
+    }
+
+    /**
+     * check for fools.
+     *
+     * @return
+     */
+    public String inSpot(){
+        return "ты дурак?";
+    }
+
+    /**
+     * gives an i derivative.
+     *
+     * @param order - order of derivative.
+     *
+     * @return
+     *
+     * <p>
+     *     takes order than multiply coef on it's position
+     * </p>
+     */
+    public Polinomial iDerivative(int order){
+        for(int i = 0; i < order; i ++){
+            for(int j = 0; j < this.polynomCof.length; j ++) {
+                this.polynomCof[j] = this.polynomCof[j] * j;
+            }
+            if(this.polynomCof.length > 1)
+                this.polynomCof = Arrays.copyOfRange(this.polynomCof, 1, this.polynomCof.length);
+        }
+        return this;
+    }
+    public Boolean isEqual(Polinomial pol2){
+        if(this.polynomCof == pol2.polynomCof)
+            return true;
+        return false;
+    }
+    public String sign(int checkSign){
+        if(checkSign > 0)
+            return "+";
+        return "-";
+    }
+    public String polToStr(){
+        String res = "";
+        if(this.polynomCof.length == 0)
+            return "ты дурак?";
+        if(this.polynomCof.length < 2) {
+            res += this.polynomCof[0];
+            return res;
+        }
+        else if(this.polynomCof.length == 2) {
+            if(this.polynomCof[1] != 0)
+                res += this.polynomCof[1] + "x" + " " + sign(polynomCof[0]) + " ";
+            res += Math.abs(this.polynomCof[0]);
+            return  res;
+        }
+        else{
+            if (this.polynomCof[this.polynomCof.length - 1] != 0)
+                res += this.polynomCof[this.polynomCof.length - 1] + "x" + "^" + Math.abs(this.polynomCof.length - 1) +
+                        " " + sign(this.polynomCof[this.polynomCof.length - 2]) + " ";
+            for (int i = this.polynomCof.length - 2; i > 1; i--) {
+                if (this.polynomCof[i] == 0)
+                    continue;
+                res += Math.abs(this.polynomCof[i]) + "x" + "^" + i + " " + sign(this.polynomCof[i - 1]) + " ";
+            }
+            if(this.polynomCof[1] != 0)
+                res += Math.abs(this.polynomCof[1]) + "x" + " " + sign(this.polynomCof[0]) + " ";
+            res += Math.abs(this.polynomCof[0]);
+            return res;
+        }
+    }
+}
---
+++ Task_1_1_2/src/test/java/ru/nsu/shushakov/SampleTest.java
@@ -0,0 +1,178 @@
+package ru.nsu.shushakov;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class SampleTest {
+
+    //тесты плюса
+    @Test
+    void checkPlus() {
+        Polinomial pol1 = new Polinomial(new int[]{1, 2, 3});
+        Polinomial pol2 = new Polinomial(new int[]{1, 2, 3, 4, 7});
+        assertArrayEquals(pol1.plus(pol2).polynomCof, new int[]{2, 4, 6, 4, 7});
+    }
+    @Test
+    void checkPlusEqual() {
+        Polinomial pol1 = new Polinomial(new int[]{1, 2, 3});
+        Polinomial pol2 = new Polinomial(new int[]{4, 5, 6});
+        assertArrayEquals(pol1.plus(pol2).polynomCof, new int[]{5, 7, 9});
+    }
+    @Test
+    void checkPlusEmpty() {
+        Polinomial pol1 = new Polinomial(new int[]{});
+        Polinomial pol2 = new Polinomial(new int[]{});
+        assertArrayEquals(pol1.plus(pol2).polynomCof, new int[]{});
+    }
+    @Test
+    void checkPlusOneNotEmpty() {
+        Polinomial pol1 = new Polinomial(new int[]{});
+        Polinomial pol2 = new Polinomial(new int[]{1, 2});
+        assertArrayEquals(pol1.plus(pol2).polynomCof, new int[]{1, 2});
+    }
+    //тесты минуса
+    @Test
+    void checkMinusAddMinus() {
+        Polinomial pol1 = new Polinomial(new int[]{1, 2, 3});
+        Polinomial pol2 = new Polinomial(new int[]{1, 2, 3, 4, 7});
+        assertArrayEquals(pol1.minus(pol2).polynomCof, new int[]{0, 0, 0, -4, -7});
+    }
+    @Test
+    void checkMinusNorm() {
+        Polinomial pol1 = new Polinomial(new int[]{1, 2, 3, 4, 7});
+        Polinomial pol2 = new Polinomial(new int[]{1, 2, 3});
+        assertArrayEquals(pol1.minus(pol2).polynomCof, new int[]{0, 0, 0, 4, 7});
+    }
+    @Test
+    void checkMinusEmpty() {
+        Polinomial pol1 = new Polinomial(new int[]{});
+        Polinomial pol2 = new Polinomial(new int[]{});
+        assertArrayEquals(pol1.minus(pol2).polynomCof, new int[]{});
+    }
+    @Test
+    void checkMinusFirstIsEmpty() {
+        Polinomial pol1 = new Polinomial(new int[]{});
+        Polinomial pol2 = new Polinomial(new int[]{1, 2, 3});
+        assertArrayEquals(pol1.minus(pol2).polynomCof, new int[]{-1, -2, -3});
+    }
+    @Test
+    void checkMinusSecondIsEmpty() {
+        Polinomial pol1 = new Polinomial(new int[]{1, 2, 3});
+        Polinomial pol2 = new Polinomial(new int[]{});
+        assertArrayEquals(pol1.minus(pol2).polynomCof, new int[]{1, 2, 3});
+    }
+    //тесты умножения
+    @Test
+    void checkMultNorm() {
+        Polinomial pol1 = new Polinomial(new int[]{5, 4, 3, 1});
+        Polinomial pol2 = new Polinomial(new int[]{1, 3, 5});
+        assertArrayEquals(pol1.mult(pol2).polynomCof, new int[]{5, 19, 40, 30, 18, 5});
+    }
+    @Test
+    void checkMultAboutMinus() {
+        Polinomial pol1 = new Polinomial(new int[]{-5, 4, 3});
+        Polinomial pol2 = new Polinomial(new int[]{1, 3});
+        assertArrayEquals(pol1.mult(pol2).polynomCof, new int[]{-5, -11, 15, 9});
+    }
+    @Test
+    void checkMultAboutAllMinus() {
+        Polinomial pol1 = new Polinomial(new int[]{-5, -4, -3});
+        Polinomial pol2 = new Polinomial(new int[]{-1, -3});
+        assertArrayEquals(pol1.mult(pol2).polynomCof, new int[]{5, 19, 15, 9});
+    }
+    @Test
+    void checkMultEmpty() {
+        Polinomial pol1 = new Polinomial(new int[]{});
+        Polinomial pol2 = new Polinomial(new int[]{});
+        assertArrayEquals(pol1.mult(pol2).polynomCof, new int[]{});
+    }
+    @Test
+    void checkMultZero() {
+        Polinomial pol1 = new Polinomial(new int[]{0});
+        Polinomial pol2 = new Polinomial(new int[]{1, 2, 3});
+        assertArrayEquals(pol1.mult(pol2).polynomCof, new int[]{0, 0, 0});
+    }
+    @Test
+    void checkSpot() {
+        Polinomial pol1 = new Polinomial(new int[]{1, 3, 5});
+        assertEquals(pol1.inSpot(1), 9);
+    }
+    @Test
+    void checkSpotEmpty() {
+        Polinomial pol1 = new Polinomial(new int[]{1, 3, 5});
+        assertEquals(pol1.inSpot(), "ты дурак?");
+    }
+    @Test
+    void checkDer() {
+        Polinomial pol1 = new Polinomial(new int[]{1, 3, 5});
+        assertArrayEquals(pol1.iDerivative(1).polynomCof, new int[]{3, 10});
+    }
+    @Test
+    void checkDerForNthng() {
+        Polinomial pol1 = new Polinomial(new int[]{1});
+        assertArrayEquals(pol1.iDerivative(1).polynomCof, new int[]{0});
+    }
+    @Test
+    void checkDer2() {
+        Polinomial pol1 = new Polinomial(new int[]{1, 3, 5});
+        assertArrayEquals(pol1.iDerivative(2).polynomCof, new int[]{10});
+    }
+    @Test
+    void checkDer3() {
+        Polinomial pol1 = new Polinomial(new int[]{1, 3, 5});
+        assertArrayEquals(pol1.iDerivative(3).polynomCof, new int[]{0});
+    }
+    @Test
+    void checkDerNeg() {
+        Polinomial pol1 = new Polinomial(new int[]{1, -2, 5});
+        assertArrayEquals(pol1.iDerivative(1).polynomCof, new int[]{-2, 10});
+    }
+    @Test
+    void checkDerZero() {
+        Polinomial pol1 = new Polinomial(new int[]{0, 0, 0});
+        assertArrayEquals(pol1.iDerivative(1).polynomCof, new int[]{0, 0});
+    }
+    @Test
+    void checkDerEmpty() {
+        Polinomial pol1 = new Polinomial(new int[]{});
+        assertArrayEquals(pol1.iDerivative(1).polynomCof, new int[]{});
+    }
+    @Test
+    void checkStrSimple() {
+        Polinomial pol1 = new Polinomial(new int[]{1});
+        assertEquals(pol1.polToStr(), "1");
+        System.out.println(pol1.toString());
+    }
+    @Test
+    void checkStrMedium() {
+        Polinomial pol1 = new Polinomial(new int[]{1, 3, 5});
+        assertEquals(pol1.polToStr(), "5x^2 + 3x + 1");
+        System.out.println(pol1.toString());
+    }
+    @Test
+    void checkStrHard() {
+        Polinomial pol1 = new Polinomial(new int[]{1, -3, 5, -10, 15});
+        assertEquals(pol1.polToStr(), "15x^4 - 10x^3 + 5x^2 - 3x + 1");
+        System.out.println(pol1.toString());
+    }
+    @Test
+    void checkStrMinusIsFirst() {
+        Polinomial pol1 = new Polinomial(new int[]{1, -3, 5, -10, -15});
+        assertEquals(pol1.polToStr(), "-15x^4 - 10x^3 + 5x^2 - 3x + 1");
+        System.out.println(pol1.toString());
+    }
+    @Test
+    void checkStrZero() {
+        Polinomial pol1 = new Polinomial(new int[]{0, 0, 0});
+        assertEquals(pol1.polToStr(), "0");
+        System.out.println(pol1.toString());
+    }
+    @Test
+    void checkStrEmpty() {
+        Polinomial pol1 = new Polinomial(new int[]{});
+        assertEquals(pol1.polToStr(), "ты дурак?");
+        System.out.println(pol1.toString());
+    }
+}
