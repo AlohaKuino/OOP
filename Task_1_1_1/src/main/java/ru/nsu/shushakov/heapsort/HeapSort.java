d4cab0802596870a016be3bc150ba53a0e093286
AlohaKuino <kolyasashatwo@gmaol.com>
2023-09-26 17:21:08 +0700

    finalll

--- Task_1_1_1/src/main/java/ru/nsu/shushakov/heapsort/HeapSort.java
+++ Task_1_1_1/src/main/java/ru/nsu/shushakov/heapsort/HeapSort.java
@@ -27,7 +27,6 @@ public class HeapSort {
     }
 
     /**
-    *
     * <p>@param arr array of ints
     * </p>
     *
@@ -61,7 +60,6 @@ public class HeapSort {
     }
 
     /**
-     * 
     * <p>@param args some magic
     * </p>
     */
