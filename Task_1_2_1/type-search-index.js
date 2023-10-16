b3de8140642e9f39273db40e3acf5d2feae37f9f
AlohaKuino <AlohaKuino@users.noreply.github.com>
2023-10-16 17:58:26 +0000

    Deploying to gh-pages from @ AlohaKuino/OOP@f6fded1e30f317210b8c4b0b8502efb8c1e559c7 🚀

---
+++ Task_1_2_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.shushakov.tree","l":"CaterpillarCrawls"},{"p":"ru.nsu.shushakov.tree","l":"SquirellCrawls"},{"p":"ru.nsu.shushakov.tree","l":"Tree"}]
