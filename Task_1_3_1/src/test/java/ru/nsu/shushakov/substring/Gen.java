88d18030e51ed543f537b70d4bc9d18b74a89409
AlohaKuino <kolyasashatwo@gmail.com>
2023-11-13 19:24:24 +0700

    editing javadocs

--- Task_1_3_1/src/test/java/ru/nsu/shushakov/substring/Gen.java
+++ Task_1_3_1/src/test/java/ru/nsu/shushakov/substring/Gen.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 /**
- * class for generating big files.
+ * generating subclass.
  */
 public class Gen {
     private final File filename;
