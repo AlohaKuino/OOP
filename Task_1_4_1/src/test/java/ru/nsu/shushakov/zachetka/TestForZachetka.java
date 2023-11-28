df353a0d730e808cd090b4a875ac288ba30b43da
AlohaKuino <127180614+AlohaKuino@users.noreply.github.com>
2023-11-28 18:12:16 +0700

    Update TestForZachetka.java

--- Task_1_4_1/src/test/java/ru/nsu/shushakov/zachetka/TestForZachetka.java
+++ Task_1_4_1/src/test/java/ru/nsu/shushakov/zachetka/TestForZachetka.java
@@ -1,12 +1,13 @@
 package ru.nsu.shushakov.zachetka;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static ru.nsu.shushakov.zachetka.SubjectAndMark.Mark._2;
-import static ru.nsu.shushakov.zachetka.SubjectAndMark.Mark._3;
-import static ru.nsu.shushakov.zachetka.SubjectAndMark.Mark._4;
-import static ru.nsu.shushakov.zachetka.SubjectAndMark.Mark._5;
+import static ru.nsu.shushakov.zachetka.SubjectAndMark.Mark.TWO;
+import static ru.nsu.shushakov.zachetka.SubjectAndMark.Mark.THREE;
+import static ru.nsu.shushakov.zachetka.SubjectAndMark.Mark.FOUR;
+import static ru.nsu.shushakov.zachetka.SubjectAndMark.Mark.FIVE;
 
 import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
@@ -16,13 +17,13 @@ import org.junit.jupiter.api.Test;
  */
 public class TestForZachetka {
     @Test
-    public void kindaFullTest() throws SemesterException {
-        SubjectAndMark a = new SubjectAndMark("OOP", _2, 1);
-        SubjectAndMark a1 = new SubjectAndMark("OOP", _2, 1);
-        SubjectAndMark b = new SubjectAndMark("OSI", _3, 2);
-        SubjectAndMark c = new SubjectAndMark("Eng", _4, 3);
-        SubjectAndMark z = new SubjectAndMark("Eng", _4, 4);
-        SubjectAndMark diploma = new SubjectAndMark("Eng", _5, "Irish accent Analysis");
+    public void testAllAreFinalCantGetUniqueDiploma() throws SemesterException {
+        SubjectAndMark a = new SubjectAndMark("OOP", TWO, 1);
+        SubjectAndMark a1 = new SubjectAndMark("OOP", TWO, 1);
+        SubjectAndMark b = new SubjectAndMark("OSI", THREE, 2);
+        SubjectAndMark c = new SubjectAndMark("Eng", FOUR, 3);
+        SubjectAndMark z = new SubjectAndMark("Eng", FOUR, 4);
+        SubjectAndMark diploma = new SubjectAndMark("Eng", FIVE, "Irish accent Analysis");
         ArrayList<SubjectAndMark> e = new ArrayList<>();
         e.add(a);
         e.add(b);
@@ -31,7 +32,7 @@ public class TestForZachetka {
         e.add(z);
         e.add(diploma);
         Zachetka d = new Zachetka("HEHE HIHI", 22213, 6, e);
-        assertEquals(2.0, d.triedToStreamButMyHeadWasUnderWater(1, 1));
+        assertEquals(2.0, d.averageBallInFromToSemesters(1, 1));
         assertTrue(d.moneyMoneyMoneyMustBeFunnyInTheRichMansWorld());
         assertEquals(3.0, d.currentAverageBall());
         assertEquals("Bruh", d.kingInTheCastle());
@@ -40,12 +41,12 @@ public class TestForZachetka {
 
     @Test
     public void exceptionTest() throws SemesterException {
-        SubjectAndMark a = new SubjectAndMark("OOP", _2, 1);
-        SubjectAndMark a1 = new SubjectAndMark("OOP", _2, 1);
-        SubjectAndMark b = new SubjectAndMark("OSI", _3, 2);
-        SubjectAndMark c = new SubjectAndMark("Eng", _4, 3);
-        SubjectAndMark z = new SubjectAndMark("Eng", _4, 4);
-        SubjectAndMark diploma = new SubjectAndMark("Eng", _5, "Irish accent Analysis");
+        SubjectAndMark a = new SubjectAndMark("OOP", TWO, 1);
+        SubjectAndMark a1 = new SubjectAndMark("OOP", TWO, 1);
+        SubjectAndMark b = new SubjectAndMark("OSI", THREE, 2);
+        SubjectAndMark c = new SubjectAndMark("Eng", FOUR, 3);
+        SubjectAndMark z = new SubjectAndMark("Eng", TWO, 4);
+        SubjectAndMark diploma = new SubjectAndMark("Eng", FIVE, "Irish accent Analysis");
         ArrayList<SubjectAndMark> e = new ArrayList<>();
         e.add(a);
         e.add(b);
@@ -58,18 +59,20 @@ public class TestForZachetka {
             Zachetka d = new Zachetka("HEHE HIHI", 22213, 9, e);
         });
 
-        String expectedMessage = "\"\\n\\n are you this old? \\n\"";
+        String expectedMessage = "\n\n are you this old? \n";
         String actualMessage = exception.getMessage();
+
+        assertEquals(expectedMessage, actualMessage);
     }
     
     @Test
-    public void falseFinal() throws SemesterException {
-        SubjectAndMark a = new SubjectAndMark("OOP", _5, 5, false);
-        SubjectAndMark a1 = new SubjectAndMark("OOP", _2, 1, false);
-        SubjectAndMark b = new SubjectAndMark("OSI", _3, 2, false);
-        SubjectAndMark c = new SubjectAndMark("Eng", _4, 3, false);
-        SubjectAndMark z = new SubjectAndMark("Eng", _2, 4, false);
-        SubjectAndMark diploma = new SubjectAndMark("Eng", _5, "Irish accent Analysis");
+    public void averageIsZero() throws SemesterException {
+        SubjectAndMark a = new SubjectAndMark("OOP", TWO, 5, false);
+        SubjectAndMark a1 = new SubjectAndMark("OOP", TWO, 1, false);
+        SubjectAndMark b = new SubjectAndMark("OSI", THREE, 2, false);
+        SubjectAndMark c = new SubjectAndMark("Eng", FOUR, 3, false);
+        SubjectAndMark z = new SubjectAndMark("Eng", TWO, 4, false);
+        SubjectAndMark diploma = new SubjectAndMark("Eng", FIVE, "Irish accent Anal,ysis");
         ArrayList<SubjectAndMark> e = new ArrayList<>();
         e.add(a);
         e.add(b);
@@ -78,6 +81,55 @@ public class TestForZachetka {
         e.add(z);
         e.add(diploma);
         Zachetka d = new Zachetka("HEHE HIHI", 22213, 5, e);
+        assertEquals("Bruh", d.kingInTheCastle());
+        assertEquals(0.0, d.averageBallInFromToSemesters(1, 1));
+        assertFalse(d.moneyMoneyMoneyMustBeFunnyInTheRichMansWorld());
         assertEquals(0.0, d.currentAverageBall());
+        assertFalse(d.moneyMoneyMoneyMustBeFunnyInTheRichMansWorld());
+    }
+
+    @Test
+    public void canGetUniqueDiploma() throws SemesterException {
+        SubjectAndMark a = new SubjectAndMark("OOP", FIVE, 1);
+        SubjectAndMark a1 = new SubjectAndMark("OOP", FIVE, 2);
+        SubjectAndMark b = new SubjectAndMark("OSI", FIVE, 3);
+        SubjectAndMark c = new SubjectAndMark("Eng", FOUR, 4);
+        SubjectAndMark z = new SubjectAndMark("Eng", FIVE, 5);
+        SubjectAndMark diploma = new SubjectAndMark("Eng", FIVE, "Irish accent Anal,ysis");
+        ArrayList<SubjectAndMark> e = new ArrayList<>();
+        e.add(a);
+        e.add(b);
+        e.add(a1);
+        e.add(c);
+        e.add(z);
+        e.add(diploma);
+        Zachetka d = new Zachetka("HEHE HIHI", 22213, 5, e);
+        assertEquals("You can do it", d.kingInTheCastle());
+        assertEquals(5.0, d.averageBallInFromToSemesters(1, 1));
+        assertTrue(d.moneyMoneyMoneyMustBeFunnyInTheRichMansWorld());
+        assertEquals(4.8, d.currentAverageBall());
+        assertTrue(d.moneyMoneyMoneyMustBeFunnyInTheRichMansWorld());
+    }
+    @Test
+    public void cantFetAnything() throws SemesterException {
+        SubjectAndMark a = new SubjectAndMark("OOP", TWO, 1);
+        SubjectAndMark a1 = new SubjectAndMark("OOP", TWO, 2);
+        SubjectAndMark b = new SubjectAndMark("OSI", TWO, 3);
+        SubjectAndMark c = new SubjectAndMark("Eng", TWO, 4);
+        SubjectAndMark z = new SubjectAndMark("Eng", TWO, 5);
+        SubjectAndMark diploma = new SubjectAndMark("Eng", TWO, "Irish accent Anal,ysis");
+        ArrayList<SubjectAndMark> e = new ArrayList<>();
+        e.add(a);
+        e.add(b);
+        e.add(a1);
+        e.add(c);
+        e.add(z);
+        e.add(diploma);
+        Zachetka d = new Zachetka("HEHE HIHI", 22213, 5, e);
+        assertEquals("Bruh", d.kingInTheCastle());
+        assertEquals(2.0 , d.averageBallInFromToSemesters(1, 1));
+        assertFalse(d.moneyMoneyMoneyMustBeFunnyInTheRichMansWorld());
+        assertEquals(2.0, d.currentAverageBall());
+        assertFalse(d.moneyMoneyMoneyMustBeFunnyInTheRichMansWorld());
     }
 }
