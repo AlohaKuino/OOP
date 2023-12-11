40a6b8d380962d8d176962d8c6b2c5a21332177c
AlohaKuino <127180614+AlohaKuino@users.noreply.github.com>
2023-12-11 15:09:11 +0700

    Update EndException.java

--- Task_1_5_1/src/main/java/ru/nsu/shushakov/calculator/EndException.java
+++ Task_1_5_1/src/main/java/ru/nsu/shushakov/calculator/EndException.java
@@ -1,6 +1,12 @@
 package ru.nsu.shushakov.calculator;
 
+/**
+ * exception to stop endless calculations.
+ */
 public class EndException extends Exception {
+    /**
+     * SUPER MESSAGE.
+     */
         public EndException() {
             super("\n\n\nStopped\n\n\n");
         }
