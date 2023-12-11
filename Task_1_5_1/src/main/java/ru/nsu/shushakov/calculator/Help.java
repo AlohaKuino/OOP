a4e1ee2cf8c19cffdb1151b0d4bc9cac9db5b7cb
AlohaKuino <127180614+AlohaKuino@users.noreply.github.com>
2023-12-11 15:08:53 +0700

    Update Help.java

--- Task_1_5_1/src/main/java/ru/nsu/shushakov/calculator/Help.java
+++ Task_1_5_1/src/main/java/ru/nsu/shushakov/calculator/Help.java
@@ -2,7 +2,15 @@ package ru.nsu.shushakov.calculator;
 
 import java.util.Scanner;
 
+/**
+ * helping class.
+ */
 public class Help {
+    /**
+     * @throws EndException if stop is printed than stop.
+     *
+     * gets a string from console.
+     */
     public static void getInputString() throws EndException {
         Scanner input = new Scanner(System.in);
         while (true) {
@@ -15,6 +23,12 @@ public class Help {
         }
     }
 
+    /**
+     * @param maybeNumber string from console.
+     * @return true if it's a number false otherwise.
+     *
+     * checks if string is a number.
+     */
     public static boolean isNumber(String maybeNumber) {
         try {
             Double.parseDouble(maybeNumber);
