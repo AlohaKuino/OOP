702b1cf7bb7be14c0cd81b49739b7391a7cac409
AlohaKuino <127180614+AlohaKuino@users.noreply.github.com>
2023-12-12 23:58:28 +0700

    Update Calculator.java

--- Task_1_5_1/src/main/java/ru/nsu/shushakov/calculator/Calculator.java
+++ Task_1_5_1/src/main/java/ru/nsu/shushakov/calculator/Calculator.java
@@ -1,123 +1,84 @@
 package ru.nsu.shushakov.calculator;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.EmptyStackException;
-import java.util.NoSuchElementException;
+import java.util.*;
 
 /**
- * main class.
+ * class that makes calculations.
  */
 public class Calculator {
-    private String inputString;
-
     /**
-     * simple constructor.
+     * calculate and parse method.
      *
-     * @throws EndException if stop is printed than stop.
+     * @param inputString string with input information.
+     * @throws EndException if stop is written.
+     * @throws WrongFormatException if format is wrong.
+     * @throws NoSuchElementException if i actually don't remember why, but i need it.
+     * @throws EmptyStackException if stack is empty, for example if we put just sin in input.
      */
-    public Calculator() throws EndException {
-        Help.getInputString();
-    }
-
-    /**
-     * main function which parses string and then calculate an expression using switch case.
-     *
-     * @param inputString string to parse.
-     *
-     * @throws EndException if stop is printed than stop.
-     */
-    public static void parseInputString(String inputString) throws EndException {
+    protected static void parseInputString(String inputString) throws EndException, WrongFormatException, NoSuchElementException, EmptyStackException {
         String[] tmpString = inputString.split(" ");
         Deque<Double> operands = new ArrayDeque<>();
         for (int i = tmpString.length - 1; i >= 0; i--) {
             if (Help.isNumber(tmpString[i])) {
                 operands.push(Double.parseDouble(tmpString[i]));
             } else {
-                try {
-                    double firstOperand = operands.pop();
-                    double secondOperand;
-                    switch (tmpString[i]) {
-                        case "sin":
-                            operands.push(Math.sin(firstOperand));
-                            break;
-                        case "cos":
-                            operands.push(Math.cos(firstOperand));
-                            break;
-                        case "log": {
-                            if (firstOperand < 0) {
-                                System.out.println("Logarithm argument must be greater than zero");
-                                operands.clear();
-                            } else {
-                                operands.push(Math.log(firstOperand));
-                            }
-                            break;
-                        }
-                        case "sqrt": {
-                            if (firstOperand < 0) {
-                                System.out.println("Sqrt argument must be greater than zero");
-                                operands.clear();
-                            } else {
-                                operands.push(Math.sqrt(firstOperand));
-                            }
-                            break;
-                        }
-                        case "pow": {
-                            secondOperand = operands.pop();
-                            if ((int) secondOperand - secondOperand != 0 && firstOperand < 0) {
-                                System.out.println("It's not a number");
-                                operands.clear();
-                            } else if (firstOperand == 0 && secondOperand < 0) {
-                                System.out.println("It's infinity");
-                                operands.clear();
-                            } else {
-                                operands.push(Math.pow(firstOperand, secondOperand));
-                            }
-                            break;
-                        }
-                        case "+": {
-                            secondOperand = operands.pop();
-                            operands.push(firstOperand + secondOperand);
-                            break;
-                        }
-                        case "-": {
-                            secondOperand = operands.pop();
-                            operands.push(firstOperand - secondOperand);
-                            break;
-                        }
-                        case "*": {
-                            secondOperand = operands.pop();
-                            operands.push(firstOperand * secondOperand);
-                            break;
-                        }
-                        case "/": {
-                            secondOperand = operands.pop();
-                            if (secondOperand == 0) {
-                                System.out.println("Division second argument must not be a zero");
-                                operands.clear();
-                            } else {
-                                operands.push(firstOperand / secondOperand);
-                            }
-                            break;
-                        }
-                        default: {
-                            System.out.println("Wrong Format");
-                            break;
-                        }
+                double firstOperand;
+                double secondOperand;
+                switch (Help.typeOfOperation(tmpString[i])) {
+                    case SIN -> {
+                        firstOperand = operands.pop();
+                        operands.push(Math.sin(firstOperand));
                     }
-                } catch (EmptyStackException | NoSuchElementException e) {
-                    if (tmpString[i].equals("stop")) {
-                        throw new EndException();
+                    case COS -> {
+                        firstOperand = operands.pop();
+                        operands.push(Math.cos(firstOperand));
                     }
-                    operands.clear();
+                    case LOG -> {
+                        firstOperand = operands.pop();
+                        operands.push(Math.log(firstOperand));
+                    }
+                    case SQRT -> {
+                        firstOperand = operands.pop();
+                        operands.push(Math.sqrt(firstOperand));
+                    }
+                    case POW -> {
+                        firstOperand = operands.pop();
+                        secondOperand = operands.pop();
+                        operands.push(Math.pow(firstOperand, secondOperand));
+                    }
+                    case PLUS -> {
+                        firstOperand = operands.pop();
+                        secondOperand = operands.pop();
+                        operands.push(firstOperand + secondOperand);
+                    }
+                    case MINUS -> {
+                        firstOperand = operands.pop();
+                        secondOperand = operands.pop();
+                        operands.push(firstOperand - secondOperand);
+                    }
+                    case MULTIPLICATION -> {
+                        firstOperand = operands.pop();
+                        secondOperand = operands.pop();
+                        operands.push(firstOperand * secondOperand);
+                    }
+                    case DIVISION -> {
+                        firstOperand = operands.pop();
+                        secondOperand = operands.pop();
+                        operands.push(firstOperand / secondOperand);
+                    }
+                    case STOP -> throw new EndException("Stop");
+                    default -> throw new WrongFormatException("Wrong Format");
                 }
             }
-
         }
-        if (operands.size() == 1) {
+        if (operands.peek().isInfinite()) {
+            System.out.println("It's infinity");
+        } else if (operands.peek().isNaN()) {
+            System.out.println("It's nan");
+        } else if (operands.size() == 1) {
             System.out.println(operands.pop());
         } else {
-            System.out.println("Wrong Format");
+            throw new WrongFormatException("Wrong Format");
         }
         operands.clear();
     }
