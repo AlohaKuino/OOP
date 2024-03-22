c6610aafaeb20f30a7c72bdea55181c450a9394c
AlohaKuino <127180614+AlohaKuino@users.noreply.github.com>
2024-03-22 22:29:22 +0700

    Update TestPizzeria.java

--- Task_2_2_1/src/test/java/ru/nsu/shushakov/pizzapepperonimario/TestPizzeria.java
+++ Task_2_2_1/src/test/java/ru/nsu/shushakov/pizzapepperonimario/TestPizzeria.java
@@ -2,39 +2,117 @@ package ru.nsu.shushakov.pizzapepperonimario;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.junit.jupiter.api.Test;
+
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-import org.junit.jupiter.api.Test;
 
 /**
- * test class.
+ * main class.
  */
 public class TestPizzeria {
+
     protected static Thread[] bakers;
     protected static Thread[] couriers;
     protected static Thread timerThread;
+    protected static Thread operatorThread;
     protected static volatile boolean pizzeriaClose = false;
 
+    /**
+     * getter.
+     *
+     * @return flag function to know if pizzeria closed.
+     */
     public static boolean isPizzeriaClose() {
         return pizzeriaClose;
     }
 
     /**
-     * method to know if pizzeria closed.
+     * main function it reads json anp put it in a class also it starts all thread and timer thread.
+     */
+    @Test
+    public void IdkWhatToTest() {
+        try {
+            String filePath = "input.json";
+            Gson gson = new Gson();
+            FileReader reader = new FileReader(filePath);
+            PizzeriaData pizzeriaData = gson.fromJson(reader, PizzeriaData.class);
+
+            Operator operator = new Operator(pizzeriaData.getOrders());
+            operatorThread = new Thread(operator);
+            operatorThread.start();
+
+            System.out.println("Time before closing: " + pizzeriaData.getTime());
+            System.out.println("Number of bakers: " + pizzeriaData.getBakers().size());
+            System.out.println("Number of couriers: " + pizzeriaData.getCouriers().size());
+            System.out.println("Maximum pizzas in warehouse: " + pizzeriaData.getWarehouse()
+                    .getCapacity());
+            System.out.println("Orders:" + pizzeriaData.getOrders().size());
+            reader.close();
+
+            pizzeriaData.setAllOrders(pizzeriaData.getOrders().size());
+            pizzeriaData.getWarehouse().setPizzaList();
+
+            bakers = new Thread[pizzeriaData.getBakers().size()];
+            couriers = new Thread[pizzeriaData.getCouriers().size()];
+
+            if(pizzeriaData.getOrders().isEmpty()){
+                System.out.println("nothing to do");
+                closePizzeria();
+            }
+
+            for (int i = 0; i < pizzeriaData.getBakers().size(); i++) {
+                bakers[i] = new BakerThread(pizzeriaData.getBakers().get(i),
+                        operator.getOrders(), pizzeriaData);
+            }
+            for (int i = 0; i < pizzeriaData.getCouriers().size(); i++) {
+                couriers[i] = new CourierThread(pizzeriaData.getCouriers().get(i),
+                        pizzeriaData, operator);
+            }
+
+            startThreads(bakers);
+            startThreads(couriers);
+
+            timerThread = startTimer(pizzeriaData.getTime());
+            timerThread.join();
+
+            joinThreads(bakers);
+            operatorThread.join();
+            joinThreads(couriers);
+            saveRemainingOrders(pizzeriaData, operator);
+            System.out.println("Pizzeria closed");
+
+        } catch (Exception e) {
+            System.out.println("oops");
+        }
+    }
+
+    /**
+     * setter that closing pizzeria.
      */
     public static void closePizzeria() {
         pizzeriaClose = true;
         System.out.println("Pizzeria should be closed");
     }
 
+    /**
+     * starts all threads in array.
+     *
+     * @param threads array of threads to start.
+     */
     private static void startThreads(Thread[] threads) {
         for (Thread thread : threads) {
             thread.start();
         }
     }
 
+    /**
+     * join all the array's threads.
+     *
+     * @param threads array of threads to join.
+     */
     private static void joinThreads(Thread[] threads) {
         for (Thread thread : threads) {
             try {
@@ -45,6 +123,12 @@ public class TestPizzeria {
         }
     }
 
+    /**
+     * starts a thread that will be sleeping for a working time.
+     *
+     * @param timeLimit time that pizzeria should work.
+     * @return thread that will be responsible for closing pizzeria in time.
+     */
     protected static Thread startTimer(long timeLimit) {
         Thread timerThread = new Thread(() -> {
             try {
@@ -58,6 +142,11 @@ public class TestPizzeria {
         return timerThread;
     }
 
+    /**
+     * saves unfinished orders back to order list in inputJSON.
+     *
+     * @param pizzeriaData object where we get all pizzeria data from.
+     */
     protected static void saveRemainingOrders(PizzeriaData pizzeriaData, Operator operator) {
         pizzeriaData.setOrders(operator.getOrderQueueForOperator());
         try (FileWriter writer = new FileWriter("input.json")) {
@@ -66,59 +155,5 @@ public class TestPizzeria {
         } catch (IOException e) {
             System.out.println("Failed to save unfinished orders: " + e.getMessage());
         }
-    }
-
-    @Test
-    public void idkWhatAndHowToTest() throws IOException, InterruptedException {
-        String filePath = "input.json";
-        Gson gson = new Gson();
-        FileReader reader = new FileReader(filePath);
-        PizzeriaData pizzeriaData = gson.fromJson(reader, PizzeriaData.class);
-
-        Operator operator = new Operator(pizzeriaData.getOrders());
-        Thread operatorThread = new Thread(operator);
-        operatorThread.start();
-
-        System.out.println("Time before closing: " + pizzeriaData.getTime());
-        System.out.println("Number of bakers: " + pizzeriaData.getBakers().size());
-        System.out.println("Number of couriers: " + pizzeriaData.getCouriers().size());
-        System.out.println("Maximum pizzas in warehouse: " + pizzeriaData.getWarehouse()
-                .getCapacity());
-        System.out.println("Orders:" + pizzeriaData.getOrders().size());
-        reader.close();
-
-        pizzeriaData.setAllOrders(pizzeriaData.getOrders().size());
-        pizzeriaData.getWarehouse().setPizzaList();
-
-        bakers = new Thread[pizzeriaData.getBakers().size()];
-        couriers = new Thread[pizzeriaData.getCouriers().size()];
-
-        for (int i = 0; i < pizzeriaData.getBakers().size(); i++) {
-            bakers[i] = new BakerThread(pizzeriaData.getBakers().get(i),
-                    operator.getOrders(), pizzeriaData);
-        }
-        for (int i = 0; i < pizzeriaData.getCouriers().size(); i++) {
-            couriers[i] = new CourierThread(pizzeriaData.getCouriers().get(i),
-                    pizzeriaData, operator);
-        }
-
-        if(pizzeriaData.getOrders().isEmpty()){
-            System.out.println("nothing to do");
-            System.exit(0);
-        }
-
-        startThreads(bakers);
-        startThreads(couriers);
-
-        timerThread = startTimer(pizzeriaData.getTime());
-
-        timerThread.join();
-
-        joinThreads(bakers);
-        joinThreads(couriers);
-
-        operatorThread.join();
-
-        System.out.println("Pizzeria closed");
     }
 }
