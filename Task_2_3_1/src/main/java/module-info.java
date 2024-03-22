c5865a84e5dab5e7e0085de1552571af573a0aac
AlohaKuino <127180614+AlohaKuino@users.noreply.github.com>
2024-03-22 22:18:49 +0700

    Delete Task_2_3_1 directory

--- Task_2_3_1/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module ru.nsu.shushakov.snake {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens ru.nsu.shushakov.snake to javafx.fxml;
-    exports ru.nsu.shushakov.snake;
-}
